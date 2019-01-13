0c32bb6930f345822e8180b40f6e52bd52741ab2
an0o0nym <an0o0nyme@gmail.com>
2019-01-13 17:10:31 +0100

    Initial code commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = map(str.strip, requirements_file.readlines())
+
+setuptools.setup(
+    name="emails-gdpr",
+    version="1.0.0",
+    author="Damian P.",
+    author_email="an0o0nyme@gmail.com",
+    description="",
+    keywords="emails gdpr data protection",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=long_description + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    url="https://github.com/an0o0nym/emails-gdpr",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Natural Language :: English',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'emails-gdpr=emails_gdpr.main:main',
+        ],
+    }
+)
