f086374d37c81fc653b9955f5ffefcbc248918eb
an0o0nym <an0o0nyme@gmail.com>
2019-01-14 21:47:48 +0100

    Bump version: 1.0.4 → 1.0.5

--- emails_gdpr/__init__.py
+++ emails_gdpr/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Damian P."""
 __email__ = 'an0o0nyme@gmail.com'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 import logging.config
 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements_file:
 
 setuptools.setup(
     name="emails-gdpr",
-    version="1.0.4",
+    version="1.0.5",
     author="Damian P.",
     author_email="an0o0nyme@gmail.com",
     description="",
